c002211a2961379aed41d2a0b0c82ee965955a57
Michael Heyman <michaelheyman@users.noreply.github.com>
2020-02-18 15:14:22 -0800

    Alias class offering return object

--- src/bin/show_joined_classofferings.rs
+++ src/bin/show_joined_classofferings.rs
@@ -8,11 +8,13 @@ use diesel::prelude::*;
 use rust_diesel::*;
 use serde_json::{json, Value};
 
+type ClassOfferingResult = (ClassOffering, Instructor, Course, Term);
+
 fn main() {
     use self::schema::*;
 
     let connection = establish_connection();
-    let query: Vec<(ClassOffering, Instructor, Course, Term)> = classoffering::table
+    let query: Vec<ClassOfferingResult> = classoffering::table
         .inner_join(instructor::table)
         .inner_join(course::table)
         .inner_join(term::table)
@@ -21,23 +23,28 @@ fn main() {
         .expect("Error loading photos");
 
     println!("Displaying {} class offerings join", query.len());
-    for (co, i, c, t) in query {
-        let json_object = create_json_object(co, i, c, t);
+    for class in query {
+        let json_object = create_json_object(class);
 
         println!("{}", serde_json::to_string_pretty(&json_object).unwrap());
     }
 }
 
-fn create_json_object(co: ClassOffering, i: Instructor, c: Course, t: Term) -> Value {
+fn create_json_object(class_offering_result: ClassOfferingResult) -> Value {
+    let class_offering = class_offering_result.0;
+    let instructor = class_offering_result.1;
+    let course = class_offering_result.2;
+    let term = class_offering_result.3;
+
     json!({
-        "id": co.id,
-        "credits": co.credits,
-        "days": co.days,
-        "time": co.time,
-        "crn": co.crn,
-        "timestamp": co.timestamp,
-        "course": c,
-        "instructor": i,
-        "term": t
+        "id": class_offering.id,
+        "credits": class_offering.credits,
+        "days": class_offering.days,
+        "time": class_offering.time,
+        "crn": class_offering.crn,
+        "timestamp": class_offering.timestamp,
+        "course": course,
+        "instructor": instructor,
+        "term": term
     })
 }
